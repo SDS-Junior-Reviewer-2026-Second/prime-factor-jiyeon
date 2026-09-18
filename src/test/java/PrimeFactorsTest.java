73cb9048aad6f624e31919a1f132a3a62e3fd715
jiyeonyooo <ji04230423@gmail.com>
2026-09-18 10:09:57 +0900

    refactor: 테스트 코드 파일 위치 변경

---
+++ src/test/java/PrimeFactorsTest.java
@@ -0,0 +1,14 @@
+import org.example.primeFactors.PrimeFactors;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PrimeFactorsTest {
+
+	@Test
+	public void testPrimefactor0f1() {
+		PrimeFactors primefactor = new PrimeFactors();
+		assertEquals(10, 10);
+	}
+
+}
